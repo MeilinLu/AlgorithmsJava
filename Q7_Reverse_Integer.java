a24e036471289c3444e5edc0ed5fe26e58cf74c7
MeilinLu <meilinlu86@gmail.com>
2019-01-07 19:28:10 -0500

    7. Reverse Integer

---
+++ Q7_Reverse_Integer.java
@@ -0,0 +1,36 @@
+/*
+7. Reverse Integer
+Given a 32-bit signed integer, reverse digits of an integer.
+
+Example 1:
+Input: 123
+Output: 321
+
+Example 2:
+Input: -123
+Output: -321
+
+Example 3:
+Input: 120
+Output: 21
+
+Note:
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+*/
+class Solution {
+    public int reverse(int x) {
+
+        long answer = 0;
+        while (x != 0) {
+            answer = answer * 10 + x % 10;
+            x /= 10;
+        }
+        if (answer > Integer.MAX_VALUE || answer < Integer.MIN_VALUE) {
+            return 0;
+        } else {
+            return (int) answer;
+        }
+        // OR
+        // return (answer > Integer.MAX_VALUE || answer < Integer.MIN_VALUE) ? 0 : (int) answer;
+    }
+}
